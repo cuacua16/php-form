68074ab4a76e624aa575f50e6cf44ba830bf00a7
cuacua <104170643+cuacua16@users.noreply.github.com>
2022-05-28 14:10:36 -0300

    Add files via upload

---
+++ desafio1/php/codigo.php
@@ -0,0 +1,8 @@
+<?php
+    $nombre = $_POST['nombre'];
+    $pass = $_POST['pass'];
+
+    echo "
+    <p><strong>Hola tu nombre es</strong>$nombre</p>
+    <p><strong>y tu contraseña es </strong>$pass</p>";
+?>
