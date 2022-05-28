423eb3bd99b749c18ecc87a93559e839d08eb914
cuacua <104170643+cuacua16@users.noreply.github.com>
2022-05-28 14:18:45 -0300

    Delete desafio1 directory

--- desafio1/php/codigo.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nombre = $_POST['nombre'];
-    $pass = $_POST['pass'];
-
-    echo "
-    <p><strong>Hola tu nombre es</strong>$nombre</p>
-    <p><strong>y tu contraseña es </strong>$pass</p>";
-?>
